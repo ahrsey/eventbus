3d97c5bb9f2132d530e23690d520397ec88cc6c2
ahrsey <richard.cargill@pm.me>
2024-05-23 18:12:08 +0100

    [not compiling] back to pub/sub

--- README.md
+++ README.md
@@ -2,12 +2,21 @@
 
 # Notes
 [sqlite3](https://gosamples.dev/sqlite-intro/)
-
 For sqlite3 implementation copy [save](../save/main.go) structure
 
 # TODO
+// TODO: Add sqlite3
+// TODO: Add subscriber to the insert into db action which will emit the
+// events, something along those lines, the idea is to use the pub/sub pattern
+// here and try not to block on posting to the api
 // TODO: Publish events
 // TODO: Filter subscriber triggers
 // TODO: Access events data in subscribers
 // TODO: When you subscribe it should create an event that is possible to be
 // trigger to be used to trigger that subscriber
+// TODO: Cleanup, we have three main libs -> pub/sub, database and http, so I think
+// it's time to tidy up before things get too messy https://github.com/Pungyeon/clean-go-article
+// TODO: Emit event
+// TODO: Pick up event and publish that event to required subscribers
+// TODO: Subscribe to publisher which creates event groups that can be
+// published to
--- main.go
+++ main.go
@@ -1,45 +1,44 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
-	"io"
-	"log"
-	"net/http"
 	"sync"
 	"time"
 )
 
 type Event struct {
-	name string
+	topic string // An event can have a single topic
+	body  string // [1]
 }
+
+// [1] An event body which will be added to the event when published and passed
+// to the subscriber
 
 type Subscriber struct {
-	name string
-	fn   func(wg *sync.WaitGroup, e Event)
+	id     string            // Used to store subscribers in a map
+	events chan *Event       // To listen for events
+	topics map[string]string // topics that this subscriber is subbed to
 }
 
-const DB_NAME = "./bus.db"
+type Broker struct {
+	subcribers map[string]string // Map of subscribers by id
+	topics     map[string]string // [2]
+}
+
+// [2] Map of topics that subscribers can subbed to, this will be used for
+// message filtering
 
 type subscribers []Subscriber
 
-// TODO: Add sqlite3 
-// TODO: Add subscriber to the insert into db action which will emit the
-// events, something along those lines, the idea is to use the pub/sub pattern
-// here and try not to block on posting to the api 
-// TODO: Cleanup, we have two main libs -> pub/sub and database, so I think
-// it's time to tidy up before things get too messy
+// TODO: Create structs: Event, Subscriber, Broker
+// TODO: Create interfaces: Event, Subscriber, Broker
 func main() {
 	e := Event{}
 	subscribers := []Subscriber{}
 
-	db := setupDatabase()
-	defer db.Close()
-
 	sub1 := Subscriber{"sub1", func(wg *sync.WaitGroup, e Event) {
 		defer wg.Done()
-		d, _ := time.ParseDuration("1s")
+		d, _ := time.ParseDuration("3s")
 		time.Sleep(d)
 		fmt.Printf("Inside sub 1\n")
 	}}
@@ -47,24 +46,15 @@ func main() {
 
 	sub2 := Subscriber{"sub2", func(wg *sync.WaitGroup, e Event) {
 		defer wg.Done()
-		d, _ := time.ParseDuration("3s")
+		d, _ := time.ParseDuration("1s")
 		time.Sleep(d)
 		fmt.Printf("Inside sub 2\n")
 	}}
 	subscribers = append(subscribers, sub2)
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("POST /", postPublish(db))
-
 	publish(subscribers, e)
-
-  err := http.ListenAndServe(":6969", mux)
-  if err != nil {
-		log.Printf("%q\n", err)
-  }
 }
 
-// Publish functions
 func publish(s []Subscriber, e Event) {
 	var wg sync.WaitGroup
 	wg.Add(len(s))
@@ -75,82 +65,4 @@ func publish(s []Subscriber, e Event) {
 	}
 
 	wg.Wait()
-}
-
-func postPublish(db *sql.DB) func(w http.ResponseWriter, r *http.Request) {
-  return func (w http.ResponseWriter, r *http.Request) {
-    name := r.URL.Query().Get("name")
-
-    insertBy(db, name, "Custom Event")
-    // publish(subscribers, e)
-    dumpDatabase(db)
-
-		// log.Printf("%q\n", r.Body)
-    io.WriteString(w, "This is my website!\n")
-  }
-}
-
-// Database functions
-func setupDatabase() *sql.DB {
-	db, err := sql.Open("sqlite3", DB_NAME)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	sqlStmt := `
-  CREATE TABLE IF NOT EXISTS events(
-    id integer not null primary key autoincrement, 
-    name text,
-    type text
-  );
-  `
-	_, err = db.Exec(sqlStmt)
-	if err != nil {
-		log.Printf("%q: %s\n", err, sqlStmt)
-		return nil
-	}
-
-	return db
-}
-
-func insertBy(db *sql.DB, name, t_type string) {
-	tx, err := db.Begin()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	stmt, err := tx.Prepare("insert into events(name, type) values(?, ?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec(name, t_type)
-	err = tx.Commit()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func dumpDatabase(db *sql.DB) {
-	rows, err := db.Query("select id, name, type from events")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var name string
-		var type_t string
-
-		err = rows.Scan(&id, &name, &type_t)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, name, type_t)
-	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
 }
